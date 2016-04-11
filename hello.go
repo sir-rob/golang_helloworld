862c818425b8eefd2c3a69ac007fc98624e089c0
Aleks Saul <aleks.saul@gmail.com>
2016-04-11 00:06:29 -0400

    Added net/http packages

--- hello.go
+++ hello.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "net/http"
+)
 
-func main() {
-	fmt.Printf("Hello, world.\n")
+const (
+  port = ":80"
+  version = "1.0"
+)
+
+var calls = 0
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+  calls++
+  fmt.Fprintf(w, "Hello, world! You have called me %d times.\nversion: %s \n", calls, version)
 }
+
+func init() {
+  fmt.Printf("Started server version:%s at http://localhost%v.\n", port, version)
+  http.HandleFunc("/", HelloWorld)
+  http.ListenAndServe(port, nil)
+}
+
+func main() {}
