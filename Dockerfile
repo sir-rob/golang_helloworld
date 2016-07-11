38e81965bb08544f9505adfe7443251c005193f9
Aleks Saul <aleks.saul@gmail.com>
2016-07-11 16:28:00 -0400

    Added Geolocation lookup support

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM golang:latest 
 RUN apt-get update
-RUN rm -rf /var/lib/apt/lists/*
-RUN mkdir /app 
+RUN rm -rf /var/lib/apt/lists/* && \
+	mkdir /app && \
+	go get github.com/oschwald/geoip2-golang
+RUN wget -q http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz && \
+	gzip -d GeoLite2-City.mmdb.gz && \
+	mv GeoLite2-City.mmdb /app
 ADD . /app/ 
 WORKDIR /app 
 RUN go build -o hello . 
--- hello.go
+++ hello.go
@@ -3,12 +3,24 @@ package main
 import (
   "fmt"
   "net/http"
-  "net"  
+  "net"
+  "os" 
+  "io/ioutil"
+  "strings"
+  "github.com/oschwald/geoip2-golang"
 )
 
 const (
   port = ":80"
-  version = "1.1"
+  version = "1.0"
+  ReportExternalIP = true
+  ReportGeoLocation = true
+)
+
+var (
+  LocalIP = GetLocalIP()
+  ExternalIP = GetExternalIP() 
+  GeoLocation = GetGeoLocation(ExternalIP) 
 )
 
 func GetLocalIP() string {
@@ -27,16 +39,54 @@ func GetLocalIP() string {
     return ""
 }
 
+func GetGeoLocation(IPAddress string) string {
+    db, err := geoip2.Open("GeoLite2-City.mmdb")
+    if err != nil {
+      os.Stderr.WriteString(err.Error())
+    }
+    defer db.Close()
+    // If you are using strings that may be invalid, check that ip is not nil
+    ip := net.ParseIP(IPAddress)
+    record, err := db.City(ip)
+    if err != nil {
+      os.Stderr.WriteString(err.Error())
+    }
+    LocationString := record.Subdivisions[0].Names["en"] + ", " + record.Country.Names["en"]
+
+    return LocationString
+}
+
+func GetExternalIP() string {
+
+  resp, err := http.Get("http://myexternalip.com/raw")
+
+  if err != nil {
+    os.Stderr.WriteString(err.Error())
+    os.Exit(1)
+  }
+
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+  return strings.TrimSpace(string(body[:]))
+}
+
 func HelloWorld(w http.ResponseWriter, r *http.Request) {  
   fmt.Fprintf(w, "Hello, world! version: %s ", version)
-  fmt.Fprintf(w, "Server IP: %s \n", GetLocalIP())
+  fmt.Fprintf(w, "Local IP: %s \n", LocalIP)
+  if ReportExternalIP ==true { fmt.Fprintf(w, "External IP: %s \n", ExternalIP) }
+  if ReportGeoLocation ==true { fmt.Fprintf(w, "Location: %s \n", GeoLocation) } 
 }
 
 func init() {
-  fmt.Printf("Started Application version:%s \n", version)
-  fmt.Printf("server IP:%s \n", GetLocalIP())  
+  fmt.Printf("Started Application version: %s \n", version)
+  fmt.Printf("Local IP: %s \n", LocalIP)  
+  if ReportExternalIP ==true { fmt.Printf("External IP: %s \n", ExternalIP) }
+  if ReportGeoLocation ==true { fmt.Printf("Location: %s \n", GeoLocation) } 
   http.HandleFunc("/", HelloWorld)
   http.ListenAndServe(port, nil)
 }
 
-func main() {}
+func main() {
+  GetExternalIP()
+  
+}
