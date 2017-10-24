a852db656c47569365314a2c441a0b49e7fd693e
Aleks Saul <aleks.saul@coreos.com>
2017-10-24 16:16:13 -0400

    split hello.go
    Move things into `pkg` es and rename hello.go to main.go

--- main.go
+++ main.go
@@ -1,25 +1,17 @@
 package main
 
 import (
-	"compress/gzip"
 	"fmt"
+	"github.com/alekssaul/golang_helloworld/pkg/location"
 	"github.com/kelseyhightower/envconfig"
-	"github.com/oschwald/geoip2-golang"
-	"io"
-	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
 	"time"
 )
 
 const (
 	version     = "3.1"
-	GeoLiteURL  = "http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz"
-	GeoLiteFile = "GeoLite2-City.mmdb"
 )
 
 var (
@@ -59,53 +51,6 @@ func GetLocalIP() string {
 	return ""
 }
 
-func GetGeoLocation(IPAddress string) (IP string, err error) {
-	_, err = os.Open(GeoLiteFile)
-	if err != nil {
-		log.Print("Could not open GeoLite DB attempting to fetch over the internet...")
-	}
-
-	err = downloadFile(GeoLiteFile+".gz", GeoLiteURL)
-	if err != nil {
-		return "", err
-	}
-	log.Print("Completed fetching GeoLite DB")
-
-	err = ungzip(GeoLiteFile+".gz", ".")
-	if err != nil {
-		return "", err
-	}
-
-	db, err := geoip2.Open(GeoLiteFile)
-
-	if err != nil {
-		return "", err
-	}
-	defer db.Close()
-
-	ip := net.ParseIP(IPAddress)
-	record, err := db.City(ip)
-	if err != nil {
-		return "", err
-	}
-	LocationString := record.Subdivisions[0].Names["en"] + ", " + record.Country.Names["en"]
-
-	return LocationString, nil
-}
-
-func GetExternalIP() (ExternalIP string, err error) {
-
-	resp, err := http.Get("http://myexternalip.com/raw")
-
-	if err != nil {
-		return "", err
-	}
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	return strings.TrimSpace(string(body[:])), err
-}
-
 func HelloWorld(w http.ResponseWriter, r *http.Request, env EnvVars, machine Machine) {
 	fmt.Fprintf(w, "Hello, world! version: %s ", version)
 	fmt.Fprintf(w, "Local IP: %s \n", machine.LocalIP)
@@ -133,55 +78,6 @@ func readiness(w http.ResponseWriter, r *http.Request, SimulateReady bool, WaitB
 	w.Write([]byte("OK"))
 }
 
-func downloadFile(filepath string, url string) (err error) {
-
-	// Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	// Get the data
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	// Writer the body to file
-	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func ungzip(source, target string) error {
-	reader, err := os.Open(source)
-	if err != nil {
-		return err
-	}
-	defer reader.Close()
-
-	archive, err := gzip.NewReader(reader)
-	if err != nil {
-		return err
-	}
-	defer archive.Close()
-
-	target = filepath.Join(target, archive.Name)
-	writer, err := os.Create(target)
-	if err != nil {
-		return err
-	}
-	defer writer.Close()
-
-	_, err = io.Copy(writer, archive)
-	return err
-}
-
 func main() {
 
 	var env EnvVars
@@ -202,21 +98,21 @@ func main() {
 	log.Printf("Local IP: %s \n", machine.LocalIP)
 
 	if env.DisplayExternalIP {
-		machine.ExternalIP, err = GetExternalIP()
+		machine.ExternalIP, err = location.GetExternalIP()
 		if err != nil {
 			log.Fatal(err.Error())
 		}
 		log.Printf("External IP: %s \n", machine.ExternalIP)
 	}
 	if env.DisplayGeoLocation {
-		machine.GeoLocation, err = GetGeoLocation(machine.ExternalIP)
+		machine.GeoLocation, err = location.GetGeoLocation(machine.ExternalIP)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
 		log.Printf("Location: %s \n", machine.GeoLocation)
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/debug", func(w http.ResponseWriter, r *http.Request) {
 		HelloWorld(w, r, env, machine)
 	})
 
---
+++ pkg/location/fetcher.go
@@ -0,0 +1,58 @@
+package location
+
+import (
+	"compress/gzip"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func DownloadFile(filepath string, url string) (err error) {
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Writer the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func UnGzip(source, target string) error {
+	reader, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+
+	archive, err := gzip.NewReader(reader)
+	if err != nil {
+		return err
+	}
+	defer archive.Close()
+
+	target = filepath.Join(target, archive.Name)
+	writer, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer writer.Close()
+
+	_, err = io.Copy(writer, archive)
+	return err
+}
